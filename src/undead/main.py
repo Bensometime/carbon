3831debf8ca4090621968f8ae9a46ac882e27561
ben <isieldor@gmail.com>
2018-11-29 14:10:18 -0600

    maturation ad puerum

--- src/undead/main.py
+++ src/undead/main.py
@@ -2,6 +2,7 @@ import os
 import time
 import board
 import NeoPixel
+import webcolors
 
 pixels = neopixel.NeoPixel(board.D18, 30)
 pixels.fill((0,0,0))
@@ -10,9 +11,6 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-
-def testboi():
-    print("yes")
 
 @app.route('/')
 def index():
@@ -31,23 +29,7 @@ def restart():
 
 @app.route('/test')
 def testguy():
-    testboi()
     return "wat"
-
-@app.route('/color/<color>')
-def redtoggle(color):
-    print("requested " + color)
-    if color == 'red':
-        print("turning lights red")
-        pixels[0] = (255,0,0)
-        pixels[1] = (255,0,0)
-        pixels[2] = (255,0,0)
-    if color != 'red':
-        print("turning lights off from /red")
-        pixels[0] = (0,0,0)
-        pixels[1] = (0,0,0)
-        pixels[2] = (0,0,0)
-    return color
 
 @app.route('/off')
 def turnoff():
@@ -55,4 +37,27 @@ def turnoff():
     pixels[0] = (0,0,0)
     pixels[1] = (0,0,0)
     pixels[2] = (0,0,0)
-    return "turning off";
+    return "turning off"
+
+@app.route('/color/<input>', methods=['GET','POST'])
+def color(input):
+    if request.method == 'POST':
+        parseinput(input)
+    if request.method == 'GET':
+        #todo: make this return the pixel states
+        return getcolor()
+        return input
+
+#my impulse to make this a single mega function for every case should be
+#supressed... so for now it will just parse the string and change lights 1-3
+#to the parsed color
+def parseinput(input):
+    newcolor = webcolors.name_to_rgb(input)
+    pixels[0] = newcolor
+    #or when that is inevitably a type error
+    pixels[0] = (newcolor.red, newcolor.green, newcolor.blue)
+    pixels[1] = (newcolor.red, newcolor.green, newcolor.blue)
+    pixels[2] = (newcolor.red, newcolor.green, newcolor.blue)
+
+def getcolor():
+    newcolor = webcolors.rgb_to_name(pixels[0][0], pixels[0][1], pixels[0][2])
